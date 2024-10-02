ec926ef59234f4040d5d400e9a9eecbfd5e9e2da
khaiky <samanthagabitano015@gmail.com>
2024-10-03 01:20:14 +0800

    Added some changes in the swapping and moving method also in deleting

---
+++ Node2.java
@@ -0,0 +1,14 @@
+Node2ackage topic_2_linkedlist_string;
+
+public class Node2 {
+    String data;  
+    Node next;    
+
+    // Constructor
+ Node2(String data) {
+        this.data = data;
+        this.next = null;
+    }
+
+}
+
